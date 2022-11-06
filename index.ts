5bd2075e2c62d91ffe4d3a468232a408abdb8025
Leif Arriens <leif.arriens@gmail.com>
2022-11-06 23:33:52 +0100

    add: automatic readme updates

---
+++ index.ts
@@ -0,0 +1,141 @@
+import fs from 'fs';
+import path from 'path';
+import axios from 'axios';
+import { Octokit } from 'octokit';
+import 'dotenv/config';
+
+const auth = process.env.ACCESS_TOKEN;
+
+const octokit = new Octokit({ auth });
+
+const ingoredLanguages = ['HTML', 'EJS', 'Pug', 'Astro', 'CSS'];
+
+type Repo = {
+  fork: boolean;
+  languages_url: string;
+  name: string;
+};
+
+type LanguageData = {
+  [key: string]: {
+    chars: number;
+    percentage: number;
+  };
+};
+
+(async () => {
+  // const json: {
+  //   repo: string;
+  //   languages: {
+  //     [key: string]: number;
+  //   };
+  // }[] = JSON.parse(fs.readFileSync('./tmp/result.json', 'utf-8'));
+
+  console.info('fetching repos...');
+
+  const { data } = await axios<{ items: Repo[] }>(
+    'https://api.github.com/search/repositories?q=user:leifarriens&per_page=100',
+    {
+      headers: {
+        Authorization: `Bearer ${auth}`,
+      },
+    },
+  );
+
+  const repos = data.items.filter((repo: Repo) => {
+    return !repo.fork;
+  });
+
+  const languagePromises = repos.map((repo) => getRepoLanguages(repo.name));
+
+  console.info('fetching repo languages...');
+
+  const languageResponses = await Promise.all(languagePromises);
+
+  console.info('transforming data...');
+
+  const languages = languageResponses
+    .map((entry) => entry.languages)
+    .filter((entry) => Object.keys(entry).length !== 0)
+    .reduce((curr, acc) => {
+      for (let key in acc) {
+        if (!curr[key]) {
+          curr[key] = acc[key];
+        } else {
+          curr[key] = curr[key] + acc[key];
+        }
+      }
+      return curr;
+    }, {});
+
+  const sortedLanguages = Object.fromEntries(
+    Object.entries(languages)
+      .filter(([name]) => {
+        return !ingoredLanguages.includes(name);
+      })
+      .sort(([, a], [, b]) => b - a),
+  );
+
+  const totalChars = Object.values(sortedLanguages).reduce((curr, acc) => curr + acc, 0);
+
+  const languagesData: LanguageData = Object.entries(sortedLanguages).reduce(
+    (curr, [name, value]) => {
+      return {
+        ...curr,
+        [name]: {
+          chars: value,
+          percentage: (value / totalChars) * 100,
+        },
+      };
+    },
+    {},
+  );
+
+  console.info('building README.md...');
+
+  const tableMarkdown = buildLanguageTable(languagesData);
+
+  const readme = parseReadmeTemplate([['{languages}', tableMarkdown]]);
+
+  fs.writeFileSync('./README.md', readme);
+  console.info('Done.');
+})();
+
+async function getRepoLanguages(repo: string) {
+  const { data } = await octokit.rest.repos.listLanguages({
+    owner: 'leifarriens',
+    repo,
+  });
+
+  return { repo, languages: data };
+}
+
+function buildLanguageTable(languagesData: LanguageData) {
+  const head = Object.keys(languagesData).reduce((curr, acc) => {
+    return `${curr}${acc}|`;
+  }, '|');
+
+  const separator = Object.keys(languagesData).reduce((curr) => {
+    return `${curr}:---:|`;
+  }, '|');
+
+  const data = Object.values(languagesData).reduce((curr, acc) => {
+    return `${curr}${acc.percentage.toFixed(2)}%|`;
+  }, '|');
+
+  const tableMarkdown = [head, separator, data].reduce((curr, acc) => {
+    return `${curr}${acc}\n`;
+  }, '');
+
+  return tableMarkdown;
+}
+
+function parseReadmeTemplate(toParse: [string, string][]) {
+  let readmeTemplate = fs.readFileSync(path.join(__dirname, 'template', 'README.md'), 'utf-8');
+
+  toParse.forEach(([key, data]) => {
+    readmeTemplate = readmeTemplate.replace(key, data);
+  });
+
+  return readmeTemplate;
+}
---
+++ template/README.md
@@ -0,0 +1,15 @@
+### Hi there 👋
+
+- 💬 Ask me about React, Express, MongoDB & HTTP APIs
+- 📫 How to reach me: [leifarriens.dev](https://leifarriens.dev)
+- ✊ Pronouns: he/him
+- ⚡ Fun fact: Svelte will replace React in the next 5 years
+- 🔍 I’m currently looking into [Remix](https://remix.run), [Astro](https://astro.build), [tRPC](https://trpc.io/)
+
+### GitHub Stats 📈
+
+![GitHub Stats](https://github-readme-stats.vercel.app/api?username=leifarriens&amp;show_icons=true)
+
+### Languages 🗒️
+
+{languages}
